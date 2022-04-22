45a464e1ce78029aefc2db91b95bf140fa250801
shadab <shadabeftekhar@gmail.com>
2022-04-22 16:54:57 +0200

    upload

--- FScars/EndPlates.py
+++ FScars/EndPlates.py
@@ -12,14 +12,14 @@ class EndPlate(GeomBase):
                    length=900,
                    height=900,
                    color="red",
-                   position= translate(rotate(self.position,"z", 90, deg=True), "y", -2000, "z", -100, "x", -400))
+                   position= translate(rotate(self.position,"z", 90, deg=True), "y", -2000, "z", 500, "x", -400))
 
     @Part
     def end_plate2(self):
         return Box(width=10,
                    length=900,
                    height=900,
-                   position= translate(rotate(translate(self.position, "y",1000), "z", 90, deg=True),"y", -2000, "z", -100, "x", -400),
+                   position= translate(rotate(translate(self.position, "y",1000), "z", 90, deg=True),"y", -2000, "z", 500, "x", -400),
                    color="red")
 
 
